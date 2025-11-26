df7bfc642d357179088919e052fe39f3b43b20e2
Jack Potter <jpotter185@gmail.com>
2025-11-25 21:52:58 -0500

    Refresh when week changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,7 +74,7 @@ export default function Home() {
     fetchCfbGames();
     const interval = setInterval(fetchCfbGames, 10000);
     return () => clearInterval(interval);
-  }, [cfbWeek]);
+  }, [cfbWeek, cfbScoreboardGroup]);
 
   let cfbFirst = true;
   const today = new Date();
