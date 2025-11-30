1bef6f4f863cb4c1c0dcd947f2ae18e97f61c0d4
Jack Potter <jpotter185@gmail.com>
2025-11-30 18:43:36 -0500

    Added collapsable live games for each league

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,8 +3,11 @@ import { useState } from "react";
 import useSWR from "swr";
 import GameCard from "./GameCard/GameCard";
 import { fetchGameStats } from "@/lib/api/games";
+import { ChevronDown } from "lucide-react";
 
 const Dashboard: React.FC = () => {
+  const [isCfbOpen, setIsCfbOpen] = useState(false);
+  const [isNflOpen, setIsNflOpen] = useState(false);
   const [openGames, setOpenGames] = useState<Record<string, boolean>>({});
 
   const toggleGame = (id: string) => {
@@ -14,34 +17,93 @@ const Dashboard: React.FC = () => {
   const { data: cfbData } = useSWR(`/api/games?league=cfb`, fetcher, {
     refreshInterval: 10000,
   });
-  const cfbGames: Game[] = cfbData?.games ?? [];
+  const cfbGames: Game[] =
+    cfbData?.games
+      .filter(
+        (game: Game) =>
+          !game.winner && !(game.gameStatus === "STATUS_SCHEDULED")
+      )
+      .sort((a: Game, b: Game) => {
+        return new Date(a.date).getTime() - new Date(b.date).getTime();
+      }) ?? [];
 
   const { data: nflData } = useSWR(`/api/games?league=nfl`, fetcher, {
     refreshInterval: 10000,
   });
-  const nflGames = nflData?.games ?? [];
-
-  const topGames = [
-    ...(cfbGames?.slice(0, 3) ?? []),
-    ...(nflGames?.slice(0, 3) ?? []),
-  ];
+  const nflGames: Game[] =
+    nflData?.games
+      .filter(
+        (game: Game) =>
+          !game.winner && !(game.gameStatus === "STATUS_SCHEDULED")
+      )
+      .sort((a: Game, b: Game) => {
+        return new Date(a.date).getTime() - new Date(b.date).getTime();
+      }) ?? [];
 
   return (
     <div className="p-4">
-      <h2 className="text-xl font-bold mb-4">Top Games</h2>
-      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-2">
-        {topGames.map((game) => {
-          return (
-            <GameCard
-              key={game.id}
-              game={game}
-              isOpen={!!openGames[game.id]}
-              toggleOpenGame={() => toggleGame(game.id)}
-              getStatsForGame={fetchGameStats}
-            />
-          );
-        })}
-      </div>
+      {cfbGames.length > 0 && (
+        <div>
+          <button
+            className="flex w-full items-center justify-between p-2 text-2xl font-bold mb-4"
+            onClick={() => setIsCfbOpen(!isCfbOpen)}
+          >
+            <span>Live CFB Games</span>
+            <ChevronDown
+              textAnchor="end"
+              className={`w-5 h-5 text-gray-500 transition-transform duration-300 ${
+                isCfbOpen ? "rotate-180" : ""
+              }`}
+            ></ChevronDown>
+          </button>
+          {isCfbOpen && (
+            <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-2">
+              {cfbGames.map((game) => {
+                return (
+                  <GameCard
+                    key={game.id}
+                    game={game}
+                    isOpen={!!openGames[game.id]}
+                    toggleOpenGame={() => toggleGame(game.id)}
+                    getStatsForGame={fetchGameStats}
+                  />
+                );
+              })}
+            </div>
+          )}
+        </div>
+      )}
+      {nflGames.length > 0 && (
+        <div>
+          <button
+            className="flex w-full items-center justify-between p-2 text-2xl font-bold mb-4"
+            onClick={() => setIsNflOpen(!isNflOpen)}
+          >
+            <span>Live NFL Games</span>
+            <ChevronDown
+              textAnchor="end"
+              className={`w-5 h-5 text-gray-500 transition-transform duration-300 ${
+                isNflOpen ? "rotate-180" : ""
+              }`}
+            ></ChevronDown>
+          </button>
+          {isNflOpen && (
+            <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-2">
+              {nflGames.map((game) => {
+                return (
+                  <GameCard
+                    key={game.id}
+                    game={game}
+                    isOpen={!!openGames[game.id]}
+                    toggleOpenGame={() => toggleGame(game.id)}
+                    getStatsForGame={fetchGameStats}
+                  />
+                );
+              })}
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
