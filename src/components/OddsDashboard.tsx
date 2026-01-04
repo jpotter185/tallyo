1e1a7b5ecec519711235bff8049bc16f3927cced
Jack Potter <jpotter185@gmail.com>
2026-01-04 17:18:28 -0500

    Fix

--- src/components/OddsDashboard.tsx
+++ src/components/OddsDashboard.tsx
@@ -6,41 +6,42 @@ import { useState } from "react";
 const OddsDashboard: React.FC = () => {
   const [isCfbOpen, setIsCfbOpen] = useState(false);
   const [isNflOpen, setIsNflOpen] = useState(false);
-  const { data: cfbData, isLoading: isCfbLoading } = useSWR(
-    `/api/games?league=cfb`,
-    fetcher,
-    {
-      refreshInterval: 30000,
-    },
-  );
-  const cfbGames: Game[] = cfbData?.games;
+  // const { data: cfbData, isLoading: isCfbLoading } = useSWR(
+  //   `/api/games/current?league=cfb`,
+  //   fetcher,
+  //   {
+  //     refreshInterval: 30000,
+  //   }
+  // );
+  // const cfbGames: Game[] = cfbData || [];
 
-  const { data: nflData, isLoading: isNflLoading } = useSWR(
-    `/api/games?league=nfl`,
-    fetcher,
-    {
-      refreshInterval: 30000,
-    },
-  );
-  const nflGames: Game[] = nflData?.games;
+  // const { data: nflData, isLoading: isNflLoading } = useSWR(
+  //   `/api/games/current?league=nfl`,
+  //   fetcher,
+  //   {
+  //     refreshInterval: 30000,
+  //   }
+  // );
+  // const nflGames: Game[] = nflData || [];
 
   return (
-    <div>
-      <LeagueOdds
-        games={cfbGames}
-        leagueName="CFB"
-        isLoading={isCfbLoading}
-        isLeagueOpen={isCfbOpen}
-        setIsLeagueOpen={setIsCfbOpen}
-      />
-      <LeagueOdds
-        games={nflGames}
-        leagueName="NFL"
-        isLoading={isNflLoading}
-        isLeagueOpen={isNflOpen}
-        setIsLeagueOpen={setIsNflOpen}
-      />
-    </div>
+    // <div>
+    //   <LeagueOdds
+    //     games={cfbGames}
+    //     leagueName="CFB"
+    //     isLoading={isCfbLoading}
+    //     isLeagueOpen={isCfbOpen}
+    //     setIsLeagueOpen={setIsCfbOpen}
+    //   />
+    //   <LeagueOdds
+    //     games={nflGames}
+    //     leagueName="NFL"
+    //     isLoading={isNflLoading}
+    //     isLeagueOpen={isNflOpen}
+    //     setIsLeagueOpen={setIsNflOpen}
+    //   />
+    // </div>
+    <div>Under construction...</div>
   );
 };
 
